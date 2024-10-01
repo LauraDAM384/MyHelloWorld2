5034a86d90892b6bccf5e4f2875a18ad9874a674
Laura <lochgon384@g.educaand.es>
2024-10-01 10:01:46 +0200

    Initial commit

---
+++ app/src/main/java/com/logde/myhelloworld/MainActivity.kt
@@ -0,0 +1,52 @@
+package com.logde.myhelloworld
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Devices
+import androidx.compose.ui.tooling.preview.Preview
+import com.logde.myhelloworld.ui.theme.MyHelloWorldTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MyHelloWorldTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true,
+    showSystemUi = true,
+    device = Devices.NEXUS_5,
+    backgroundColor = 345
+)
+@Composable
+fun GreetingPreview() {
+    MyHelloWorldTheme {
+        Greeting("Android")
+    }
+}
